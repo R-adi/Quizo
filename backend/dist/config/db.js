59cbd86bfeaabf0518c17143733a4582e2a43bcb
R-adi <103630449+R-adi@users.noreply.github.com>
2025-02-16 23:29:54 +0530

    updated

--- backend/dist/config/db.js
+++ backend/dist/config/db.js
@@ -2,10 +2,8 @@
 import pg from 'pg';
 import 'dotenv/config';
 const pool = new pg.Pool({
-    user: process.env.DB_USER || 'postgres', // Default user
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'quizo',
-    password: process.env.DB_PASSWORD || 'your_password', // Password you set during installation
-    port: parseInt(process.env.DB_PORT || '5432'),
+    // Password you set during installation
+    connectionString: process.env.DATABASE_URL, // Use the connection string
+    ssl: { rejectUnauthorized: false }, // Required for Render
 });
 export default pool;
